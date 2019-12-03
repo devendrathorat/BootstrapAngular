2ba627d36a119e037fea748a92a3e54fa2ad3964
Thorat <devendra.thorat@tatatechnologies.com>
2019-12-03 11:10:11 +0530

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  // "styles": [
+  //   "./header.component.css",
+  //   "node_modules/bootstrap/dist/css/bootstrap.min.css"
+  // ],
+  
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  public LOGO = require("../eLogo.png");
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
